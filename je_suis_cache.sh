eb5409555370847e4ac9b8f45c358939a53687a4
abracadamain <163309324+abracadamain@users.noreply.github.com>
2024-11-13 10:21:37 +0100

    timer

--- je_suis_cache.sh
+++ je_suis_cache.sh
@@ -1,2 +1,10 @@
 #!bin/bash
-
+duration=10
+# Lancer le script de compte à rebours en arrière-plan
+if [ -f countdown.sh ]; then
+    rm -f ./.countdown_expired
+    ./countdown.sh $duration &
+    echo "in progress" > ./.countdown_in_progress
+else
+    echo "Le script countdown.sh n'existe pas."
+fi
